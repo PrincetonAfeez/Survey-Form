dac5523f6b98190193db1001bfeaa3fdf436bae5
Princeton Afeez <princetonafeez@gmail.com>
2026-05-20 12:05:50 -0700

    Production settings for surveys app

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,3 +1,5 @@
+""" Production settings for surveys app """
+
 from django.core.exceptions import ImproperlyConfigured
 
 from .base import *  # noqa: F403
@@ -9,15 +11,6 @@ DEBUG = False
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 if not ALLOWED_HOSTS:
     raise ImproperlyConfigured("Set ALLOWED_HOSTS in the environment for production.")
-
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-# Preload list requires max-age >= 31536000 (1 year); see https://hstspreload.org/
-SECURE_HSTS_SECONDS = 60 * 60 * 24 * 365
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_HSTS_PRELOAD = True
 
 LOGGING = {
     "version": 1,
